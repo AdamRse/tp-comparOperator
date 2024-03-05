71e087f72ea1b1d8421cbf5c1d6d57d10d089403
Willy Boulhol <wboulhol@gmail.com>
2024-03-05 09:15:20 +0100

    ..

--- htdocs/pages/index/actions/default.php
+++
@@ -1,2 +0,0 @@
-
-<canvas id="canvas" data-engine="three.js r146"></canvas>
--- htdocs/php_config/autoload.php
+++
@@ -1,6 +0,0 @@
-<?php
-function autoloading($class){
-    $path = $_SERVER['DOCUMENT_ROOT']."/classes/$class.php";
-    require $path;
-}
-spl_autoload_register("autoloading");
